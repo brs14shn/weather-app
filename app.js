2da5ffe1f565558d446e1ae332277ecd45e18472
brs14shn <brsshn292929@gmail.com>
2022-06-16 15:50:46 +0300

    try catch

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const search=document.querySelector("button");
 
 const input=document.querySelector("input")
@@ -7,10 +9,25 @@ const containerDiv=document.querySelector(".container");
 
 
 
-const getWeatherInfo=()=>{
+const getWeatherInfo=async ()=>{
 
     const key="346b0899c9b7d0f52312e560ac10f74e"
     const url=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric&appid=${key}`
+
+
+    try{
+
+        const response=await fetch(url);
+        console.log(response)
+        const weatherInfo=await response.json()
+        console.log(weatherInfo);
+        const {weather,main,name}=weatherInfo
+        containerDiv.innerHTML=`${main.temp}${name}${weather[0].description} <img src="http://openweathermap.org/img/wn/10d@2x.png"`
+    }
+    catch(error){
+        console.log(error);
+
+    }
 
 
 
