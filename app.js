f2fc2d9cd68c4d48eb0633f2f125244f6f4af0a8
brs14shn <brsshn292929@gmail.com>
2022-06-16 18:52:18 +0300

    style.css updated

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const getWeatherInfo=async ()=>{
         const weatherInfo=await response.json()
         console.log(weatherInfo);
         const {weather,main,name}=weatherInfo
-        containerDiv.innerHTML+=`<p>${name}</p><p>${main.temp.toPrecision(2)}</p><img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png"/><p>${weather[0].description}</p> `
+        containerDiv.innerHTML+=`<div class="weather_info"><p class="city_name">${name}</p><p class="temperature">${main.temp.toPrecision(2)}&degC</p><img src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png"/><p>${weather[0].description}</p></div> `
         input.value="";
     }
     catch(error){
