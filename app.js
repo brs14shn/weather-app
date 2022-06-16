c5979b74b36878f56b1e91a42b204d4848421b0f
brs14shn <brsshn292929@gmail.com>
2022-06-16 15:12:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const search=document.querySelector("button");
+
+const input=document.querySelector("input")
+
+
+const containerDiv=document.querySelector(".container");
+
+
+
+const getWeatherinfo=()=>{
+
+
+
+
+}
+search.addEventListener("click",getWeatherinfo);
